8da39643d43fce570c8048f8dd0aef95587a231e
Guilherme <guilhermeym2009@gmail.com>
2024-03-24 13:43:32 -0300

    Update [id].jsx

--- my-app/src/pages/task/[id].jsx
+++ my-app/src/pages/task/[id].jsx
@@ -3,33 +3,46 @@ import React, { useEffect, useState } from "react";
 import "@/app/globals.css";
 import { useRouter } from "next/router";
 import useAPI from "@/app/hooks/useAPI";
-const taskView = () => {
-  const { query } = useRouter();
+
+const TaskView = () => {
+  const router = useRouter();
   const [darkMode, setDarkMode] = useState(false);
   const [taskContent, setTaskContent] = useState("");
   const { viewTask } = useAPI();
-  const taskID = query?.id;
+  const { id } = router.query;
+
   useEffect(() => {
+    // Verificar o modo escuro
     const verificateDarkMode = JSON.parse(localStorage.getItem("darkMode"));
     if (verificateDarkMode) {
       setDarkMode(!darkMode);
       document.body.classList.toggle("bg-black", verificateDarkMode);
     }
+
+    // Buscar a tarefa com base no ID
     const setContent = async () => {
       try {
         const viewTaskVerificate = await viewTask();
         if (viewTaskVerificate.data && viewTaskVerificate.data.user) {
           const arrayTask = viewTaskVerificate.data.user;
-          console.log(arrayTask);
+
+          // Encontrar a tarefa com base no ID
+          const foundTask = arrayTask.find(task => task.idTask === parseInt(id));
+          if (foundTask) {
+            setTaskContent(foundTask.content);
+          } else {
+            console.log("Tarefa não encontrada");
+          }
         } else {
-          console.log(new Error(`UseEffect`));
+          console.log(new Error(`UseEffect: Não há dados de usuário`));
         }
       } catch (err) {
         console.log(new Error(err));
       }
     };
-    setContent()
-  }, []);
+    setContent();
+  }, [id]); 
+
   return (
     <>
       <header>
@@ -38,11 +51,14 @@ const taskView = () => {
       <main>
         <div className="flex flex-col justify-center items-center">
           <h2>Tarefa</h2>
-          <div></div>
+          <div>
+            <p>Conteúdo da task: {taskContent}</p>
+            <p>Id da Task: {id}</p>
+          </div>
         </div>
       </main>
     </>
   );
 };
 
-export default taskView;
+export default TaskView;
