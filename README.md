9f116c4f0f97c525a8585726ab6bcb2f0ca515ff
Guilherme <guilhermeym2009@gmail.com>
2024-03-24 13:53:05 -0300

    Escrevendo algumas coisas no readme

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # Gerenciador de tarefas
- Gerenciador de tarefas usando next, tailwind, entre outros
 
+Gerenciador de tarefas usando next, tailwind, entre outros
+
+## JavaScript
+
+Eu utilizei JavaScript para estar montando a API e o página também, porque acabei utilizando o React e o Next
+
+### Iniciando a API e o front-end
+
+Para inicializar a Api, você precisa acessar a pasta **BackEnd** e usar o comando:
+
+```
+npm start
+
+```
+
+Já a parte do front-end, você pode utilizar o comando:
+
+```
+npm run dev
+
+```
+## Css
+
+Na parte da estilização eu usei o **Tailwind** e eu não fiz muita coisa, fiz apenas o básico, porque também eu não sou tão bom assim na parte da estilização 
+
+
+**Por enquanto é só isso**
--- my-app/src/pages/task/[id].jsx
+++ my-app/src/pages/task/[id].jsx
@@ -8,6 +8,7 @@ const TaskView = () => {
   const router = useRouter();
   const [darkMode, setDarkMode] = useState(false);
   const [taskContent, setTaskContent] = useState("");
+  const [taskTitle, setTaskTitle] = useState("")
   const { viewTask } = useAPI();
   const { id } = router.query;
 
@@ -30,8 +31,7 @@ const TaskView = () => {
           const foundTask = arrayTask.find(task => task.idTask === parseInt(id));
           if (foundTask) {
             setTaskContent(foundTask.content);
-          } else {
-            console.log("Tarefa não encontrada");
+            setTaskTitle(foundTask.title)
           }
         } else {
           console.log(new Error(`UseEffect: Não há dados de usuário`));
@@ -52,6 +52,7 @@ const TaskView = () => {
         <div className="flex flex-col justify-center items-center">
           <h2>Tarefa</h2>
           <div>
+            <p>Título da task: {taskTitle}</p>
             <p>Conteúdo da task: {taskContent}</p>
             <p>Id da Task: {id}</p>
           </div>
